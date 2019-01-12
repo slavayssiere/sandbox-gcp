cc7d24bad5a67f2120f806c74e01f900b652e28a
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-01-12 07:53:28 +0100

    solve delete SA issue

--- iac/layer-base/destroy.sh
+++ iac/layer-base/destroy.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 REGION="europe-west1"
 SUBNET_EUROPE1_CIDR="192.168.0.0/20"
+GCP_PROJECT="slavayssiere-sandbox"
 
-gcloud config set project slavayssiere-sandbox
+gcloud config set project $GCP_PROJECT
 gcloud config set compute/region $REGION
 
 gcloud -q beta compute routers nats delete nat-$REGION --router=nat-$REGION
@@ -11,7 +14,7 @@ gcloud -q beta dns managed-zones delete private-dns-zone
 terraform destroy \
     -auto-approve
 
-gcloud -q iam service-accounts delete "sa-pubsub-publisher"
-gcloud -q iam service-accounts delete "sa-pubsub-subscriber"
-gcloud -q iam service-accounts delete "sa-pubsub-full"
-gcloud -q iam service-accounts delete "sa-aggregator"
+gcloud -q iam service-accounts delete "sa-pubsub-publisher@$GCP_PROJECT.iam.gserviceaccount.com"
+gcloud -q iam service-accounts delete "sa-pubsub-subscriber@$GCP_PROJECT.iam.gserviceaccount.com"
+gcloud -q iam service-accounts delete "sa-pubsub-full@$GCP_PROJECT.iam.gserviceaccount.com"
+gcloud -q iam service-accounts delete "sa-aggregator@$GCP_PROJECT.iam.gserviceaccount.com"
