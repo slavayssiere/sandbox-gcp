5bfa4bffeae68fc23342f8f3b5001a482f44f5a0
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-01-12 07:56:15 +0100

    trash old thing

--- app-grpc/iac/deploy-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl create -f ../consumer/consumer-deploy.yaml
-kubectl create -f ../normalizer/normalizer-deploy.yaml
-kubectl create -f ../bigtable/bigtable-load-deploy.yaml
-
-kubectl create -f ../injector/
--- app-grpc/iac/undeploy-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl delete -f ../consumer/consumer-deploy.yaml
-kubectl delete -f ../normalizer/normalizer-deploy.yaml
-kubectl delete -f ../bigtable/bigtable-load-deploy.yaml
-
-kubectl delete -f ../injector/
-
