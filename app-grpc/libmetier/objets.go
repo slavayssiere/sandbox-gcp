82415783aaf847bdc18e56f214a19a0b92491277
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2019-01-14 12:53:57 +0100

    add timestamp

--- app-grpc/libmetier/objets.go
+++ app-grpc/libmetier/objets.go
@@ -1,8 +1,11 @@
 package libmetier
 
+import "time"
+
 // MessageSocial a common social msg
 type MessageSocial struct {
-	Data   string `json:"data"`
-	User   string `json:"user"`
-	Source string `json:"source"`
+	Data   string    `json:"data"`
+	User   string    `json:"user"`
+	Source string    `json:"source"`
+	Date   time.Time `json:"timestamp"`
 }
